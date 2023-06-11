afca52d64817946e572a19e3077dd5498e1003cb
karbolinivan <karboliniv@gmail.com>
2023-06-11 15:46:52 +0300

    pytest marks

--- tests/games/reference/languages/test_get_languages.py
+++ tests/games/reference/languages/test_get_languages.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 import requests
 
 from tests.games.constants import BASE_URL, LANGUAGES_LIST_URL
@@ -6,6 +7,7 @@ from source.base.validator import ResponseValidator
 from source.schemas.laguage_schema import Language
 
 
+@pytest.mark.regression
 @allure.story('Проверка успешного ответа [200] при запросе списка языков.')
 def test_get_languages_list():
     url = BASE_URL + LANGUAGES_LIST_URL
