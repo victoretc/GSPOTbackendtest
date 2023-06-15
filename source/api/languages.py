db6edd1fe3b92fbea9c5aa2846d1d545d7f15367
karbolinivan <karboliniv@gmail.com>
2023-06-16 01:56:01 +0300

    added enum for games

--- source/api/languages.py
+++ source/api/languages.py
@@ -2,18 +2,21 @@ import allure
 
 from source.base.client import Requests
 from constants import tester_auth
-from tests.games.constants import BASE_URL, LANGUAGES_URL, LANGUAGES_LIST_URL
+from source.enums.games import Reference
 
 
 @allure.step('Get languages list')
-def get_languages_list(auth=tester_auth):
-    url = BASE_URL + LANGUAGES_LIST_URL
-    response = Requests.get(url=url, auth=auth)
+def get_languages_list(limit: int = None, offset: int = None, auth=tester_auth):
+    params = {
+        'limit': limit,
+        'offset': offset
+    }
+    response = Requests.get(url=Reference.LANGUAGES, params=params, auth=auth)
     return response
 
 
 @allure.step('Create a language')
-def create_language(body, auth=tester_auth):
-    url = BASE_URL + LANGUAGES_URL
-    response = Requests.post(url=url, json=body, auth=auth)
+def create_languages(body, auth=tester_auth):
+    response = Requests.post(url=Reference.LANGUAGES, json=body, auth=auth)
     return response
+
