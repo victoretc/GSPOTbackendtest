8542c072aff404943a43fbd7554b44d8a28d7046
karbolinivan <karboliniv@gmail.com>
2023-06-16 03:42:07 +0300

    added more api for languages

--- source/api/languages.py
+++ source/api/languages.py
@@ -1,8 +1,15 @@
 import allure
 
-from source.base.client import Requests
 from constants import tester_auth
+from source.base.client import Requests
 from source.enums.games import Reference
+
+
+@allure.step('Get languages with id "{languages_id}"')
+def get_languages(languages_id: int, auth=tester_auth):
+    url = f'{Reference.LANGUAGES}{languages_id}'
+    response = Requests.get(url=url, auth=auth)
+    return response
 
 
 @allure.step('Get languages list')
@@ -20,3 +27,23 @@ def create_languages(body, auth=tester_auth):
     response = Requests.post(url=Reference.LANGUAGES, json=body, auth=auth)
     return response
 
+
+@allure.step('Update the language')
+def update_languages(languages_id: int, body, auth=tester_auth):
+    url = f'{Reference.LANGUAGES}{languages_id}'
+    response = Requests.put(url=url, json=body, auth=auth)
+    return response
+
+
+@allure.step('Update the language partly')
+def update_languages_partly(languages_id: int, body, auth=tester_auth):
+    url = f'{Reference.LANGUAGES}{languages_id}'
+    response = Requests.patch(url=url, json=body, auth=auth)
+    return response
+
+
+@allure.step('Delete the language')
+def delete_languages(languages_id: int, auth=tester_auth):
+    url = f'{Reference.LANGUAGES}{languages_id}'
+    response = Requests.delete(url=url, auth=auth)
+    return response
