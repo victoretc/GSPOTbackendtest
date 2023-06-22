e2ca2459eee02b50f1d80bd94187fe42e1aeedca
karbolinivan <karboliniv@gmail.com>
2023-06-22 14:12:02 +0300

    tests genre

--- tests/games/conftest.py
+++ tests/games/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
+from source.api.genre import create_genre, delete_genre
 from source.api.languages import create_languages, delete_languages
 from source.base.generator import Generator
+from source.schemas.genre_schema import Genre
 from source.schemas.laguage_schema import Language
 from source.base.validator import assert_status_code
 
@@ -21,5 +23,24 @@ def create_delete_test_languages():
 def create_test_languages():
     payload = Generator.object(model=Language, exclude="id")
     response = create_languages(json=payload)
+    assert_status_code(response=response, expected=201)
+    return response
+
+
+@pytest.fixture()
+def create_delete_test_genre():
+    payload = Generator.object(model=Genre, exclude="id")
+    response = create_genre(json=payload)
+    assert_status_code(response=response, expected=201)
+    id_test = response.json().get('id')
+    yield response
+    response = delete_genre(id_data=id_test)
+    assert_status_code(response=response, expected=204)
+
+
+@pytest.fixture()
+def create_test_genre():
+    payload = Generator.object(model=Genre, exclude="id")
+    response = create_genre(json=payload)
     assert_status_code(response=response, expected=201)
     return response
--- tests/games/reference/genre/test_get_genre.py
+++ tests/games/reference/genre/test_get_genre.py
@@ -1,18 +1,29 @@
 import allure
+import pytest
 
-from source.api.genre import get_genre_list
 from source.schemas.genre_schema import Genre
-from source.base.validator import assert_status_code, assert_json_by_model
+from source.api.genre import get_genre_list, get_genre
+from source.base.validator import assert_status_code, assert_json_by_model, assert_json_key_value
 
 
 @allure.epic('Games')
 @allure.feature('Reference')
 @allure.story('Genre')
-class TestGetGenre:
+@allure.suite('Test get genre')
+@pytest.mark.smoke
+class TestGenre:
 
     @allure.title('Test genre list')
     @allure.description('Проверка успешного ответа [200] при запросе списка жанров')
-    def test_get_genre_list(self):
+    def test_genre_list(self):
         response = get_genre_list()
         assert_status_code(response=response, expected=200)
         assert_json_by_model(response=response, model=Genre)
+
+    @allure.title('Test genre read')
+    @allure.description('Проверка успешного ответа [200] при запросе жанра по ID.')
+    def test_genre_read(self, create_delete_test_genre):
+        id_test = create_delete_test_genre.json().get('id')
+        response = get_genre(id_data=id_test)
+        assert_status_code(response=response, expected=200)
+        assert_json_key_value(response=response, json=create_delete_test_genre.json(), key='id')
